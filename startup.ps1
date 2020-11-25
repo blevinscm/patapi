cb83b81048546ace7a87d3c0350955fbb6a005a2
blevinscm <blevinscm@gmail.com>
2020-11-25 14:59:00 -0500

    Fixed Startup files

--- startup.ps1
+++ startup.ps1
@@ -1,1 +1,1 @@
-uvicorn app.main:app --reload
+python -m uvicorn app.main:app --host 0.0.0.0
--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-uvicorn app.main:app --reload
+python -m uvicorn app.main:app --host 0.0.0.0
